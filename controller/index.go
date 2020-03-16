148fc93b335751a283ba9370214ed1b8a50b8f45
ohko <ohko@qq.com>
2020-03-16 23:30:45 +0800

    "github.com/gorilla/websocket"

--- controller/index.go
+++ controller/index.go
@@ -21,33 +21,33 @@ func (o *IndexController) Index(ctx *hst.Context) {
 
 // Ws websocket例子
 func (o *IndexController) Ws(ctx *hst.Context) {
-	/*
-		// 建立连接
-		ws, err := websocket.Upgrade(ctx.W, ctx.R, nil, 1024, 1024)
-		if err != nil {
-			log.Println(err)
-			http.NotFound(ctx.W, ctx.R)
-			ctx.Close()
-		}
+	/* "github.com/gorilla/websocket"
+	// 建立连接
+	ws, err := websocket.Upgrade(ctx.W, ctx.R, nil, 1024, 1024)
+	if err != nil {
+		log.Println(err)
+		http.NotFound(ctx.W, ctx.R)
+		ctx.Close()
+	}
 
-		// 接收数据
-		go func() {
-			var msg hst.JSONData
-			for {
-				if err := ws.ReadJSON(&msg); err != nil {
-					common.LL.Log2Error(err)
-					break
-				}
-				common.LL.Log0Debug(msg.No, msg.Data)
-			}
-		}()
-
-		// 发送数据
+	// 接收数据
+	go func() {
+		var msg hst.JSONData
 		for {
-			time.Sleep(time.Second * 3)
-			if err := ws.WriteJSON(hst.JSONData{No: 0, Data: "ok"}); err != nil {
+			if err := ws.ReadJSON(&msg); err != nil {
+				common.LL.Log2Error(err)
 				break
 			}
+			common.LL.Log0Debug(msg.No, msg.Data)
 		}
+	}()
+
+	// 发送数据
+	for {
+		time.Sleep(time.Second * 3)
+		if err := ws.WriteJSON(hst.JSONData{No: 0, Data: "ok"}); err != nil {
+			break
+		}
+	}
 	// */
 }
