855a1002d16c20fdb73303a1b68a856b5e6f73c7
ohko <ohko@qq.com>
2020-09-15 11:04:00 +0800

    优化vue路由配置

--- vue/core/route.js
+++ vue/core/route.js
@@ -1,39 +1,20 @@
 // 路由
-
-import Dashboard from '../pages/dashboard.vue'
-import Login from '../pages/login.vue'
-import Logout from '../pages/logout.vue'
-
-import Form from '../pages/form.vue'
-import Table from '../pages/table.vue'
-import Password from '../pages/password.vue'
-import Success from '../pages/success.vue'
-import Error from '../pages/error.vue'
-
-import AdminUserList from '../user/list.vue'
-import AdminUserAdd from '../user/add.vue'
-import AdminUserEdit from '../user/edit.vue'
-
-import AdminSettingList from '../setting/list.vue'
-import AdminSettingAdd from '../setting/add.vue'
-import AdminSettingEdit from '../setting/edit.vue'
-
 module.exports = [
-   { path: '/', component: Dashboard },
-   { path: '/admin', component: Dashboard },
-   { path: '/admin/dashboard', component: Dashboard },
-   { path: '/admin/login', name: "login", component: Login },
-   { path: '/admin/logout', component: Logout },
-   { path: '/admin/form', component: Form },
-   { path: '/admin/table', component: Table },
-   { path: '/admin/password', component: Password },
-   { path: '/admin/success', component: Success },
-   { path: '/admin/error', component: Error },
-   { path: '/admin/user/list', component: AdminUserList },
-   { path: '/admin/user/add', component: AdminUserAdd },
-   { path: '/admin/user/edit/:id', component: AdminUserEdit },
-   { path: '/admin/setting/list', component: AdminSettingList },
-   { path: '/admin/setting/add', component: AdminSettingAdd },
-   { path: '/admin/setting/edit/:key', component: AdminSettingEdit },
-   { path: '*', component: Error },
+   { path: '/', component: _ => import('../pages/dashboard.vue') },
+   { path: '/admin', component: _ => import('../pages/dashboard.vue') },
+   { path: '/admin/dashboard', component: _ => import('../pages/dashboard.vue') },
+   { path: '/admin/login', name: "login", component: _ => import('../pages/login.vue') },
+   { path: '/admin/logout', component: _ => import('../pages/logout.vue') },
+   { path: '/admin/form', component: _ => import('../pages/form.vue') },
+   { path: '/admin/table', component: _ => import('../pages/table.vue') },
+   { path: '/admin/password', component: _ => import('../pages/password.vue') },
+   { path: '/admin/success', component: _ => import('../pages/success.vue') },
+   { path: '/admin/error', component: _ => import('../pages/error.vue') },
+   { path: '/admin/user/list', component: _ => import('../user/list.vue') },
+   { path: '/admin/user/add', component: _ => import('../user/add.vue') },
+   { path: '/admin/user/edit/:id', component: _ => import('../user/edit.vue') },
+   { path: '/admin/setting/list', component: _ => import('../setting/list.vue') },
+   { path: '/admin/setting/add', component: _ => import('../setting/add.vue') },
+   { path: '/admin/setting/edit/:key', component: _ => import('../setting/edit.vue') },
+   { path: '*', component: _ => import('../pages/error.vue') },
 ]
