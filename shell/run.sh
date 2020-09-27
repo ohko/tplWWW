cd0977fabc8de77a548b431cfe19682e66e00a89
ohko <ohko@qq.com>
2020-09-27 13:33:54 +0800

    .

--- shell/run.sh
+++ shell/run.sh
@@ -4,7 +4,6 @@ docker pull registry.cn-chengdu.aliyuncs.com/zx-tech/tpler
 docker rm -fv tpler && \
 docker run -d --name=tpler --restart=always \
    -p 8079:8080 \
-   -v /data/ebayerp/data/session:/session \
    -v /usr/share/zoneinfo:/usr/share/zoneinfo:ro \
    -v /etc/ssl/certs:/etc/ssl/certs:ro \
    -e TZ=Asia/Shanghai \
